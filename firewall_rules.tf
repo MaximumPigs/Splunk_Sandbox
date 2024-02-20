4b39c42256721fbe299c47355a9c9e23a2950b37
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-20 19:45:55 +1100

    Add a second node

--- firewall_rules.tf
+++ firewall_rules.tf
@@ -16,16 +16,14 @@ resource "aws_vpc_security_group_egress_rule" "all" {
 resource "aws_vpc_security_group_ingress_rule" "ssh_inbound_runner_ip" {
   count             = var.runner_access_enabled == true ? 1 : 0
   security_group_id = aws_security_group.security_group.id
-  description       = "SSH Inbound from runner and my OP"
-  from_port         = 22
+  description       = "All Inbound from runner and my IP"
   cidr_ipv4         = "${chomp(data.http.myip.response_body)}/32"
-  to_port           = 22
-  ip_protocol       = "tcp"
+  ip_protocol       = -1
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ssh_inbound_my_ip" {
   security_group_id = aws_security_group.security_group.id
-  description       = "SSH Inbound from runner and my OP"
+  description       = "SSH Inbound from runner and my IP"
   from_port         = 22
   cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
   to_port           = 22
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "instance_ip" {
-  value = aws_instance.my_instance.public_ip
+output "instance_ip_0" {
+  value = aws_instance.my_instance[0].public_ip
+}
+
+output "instance_ip_2" {
+  value = aws_instance.my_instance[1].public_ip
 }
