656e5c78f54c26c5df0fa634d625ab19ca923d2c
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-21 15:28:59 +1100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "instance_ip_0" {
-  value = aws_instance.my_instance[0].public_ip
-}
-
-output "instance_ip_2" {
-  value = aws_instance.my_instance[1].public_ip
+output "instance_ips" {
+  value = [for instance in aws_instance.my_instance : instance.public_ip]
 }
